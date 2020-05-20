6e10cb4f14246806d19b8442a0f9cf1995997cd0
ayush6624 <ayushg1214@gmail.com>
2020-05-20 18:09:12 +0530

    feat: add Google Analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,27 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-
+let GA_TRACKING_ID = 'UA-134274140-1'
+if (process.env.NODE_ENV !== 'production') GA_TRACKING_ID = ''
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head />
+        <>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${GA_TRACKING_ID}');
+      `,
+            }}
+          />
+        </>
         <body>
           <Main />
           <NextScript />
